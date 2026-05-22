a41565114c99bbdd7b3d6389edc46e5f36643042
buhmann <buhmann81@gmail.com>
2026-05-22 12:44:46 +0200

    ajax toolbar update; implement common component for update

--- view/frontend/web/js/product/list/toolbar.js
+++ view/frontend/web/js/product/list/toolbar.js
@@ -1,83 +1,126 @@
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 define([
-    'jquery'
-], function ($) {
+    'jquery',
+    'Buhmann_Catalog/js/navigation-pool'
+], function ($, navigationPool) {
     'use strict';
 
     return function (targetWidget) {
         $.widget('mage.productListToolbarForm', targetWidget, {
             options: {
                 ajaxNavigation: false,
-                mainContentSelector: '.columns',
-                productWrapperSelector: '.products.list.items',
                 paginationSelector: '.pages',
-                toolbarSelector: '.toolbar-products',
             },
+
+            _create: function () {
+                if (!this.options.ajaxNavigation) {
+                    return this._super();
+                }
+
+                const self = this;
+
+                // 1. Pagination links click handling
+                $('body').off('click', this.options.paginationSelector + ' a')
+                    .on('click', this.options.paginationSelector + ' a', function (e) {
+                        e.preventDefault();
+                        e.stopImmediatePropagation();
+                        const url = $(this).attr('href');
+                        if (url) {
+                            navigationPool.navigate(url, false);
+                        }
+                    });
+
+                // 2. Grid/List mode switcher
+                $('body').off('click', this.options.modeControl)
+                    .on('click', this.options.modeControl, function (e) {
+                        e.preventDefault();
+                        e.stopImmediatePropagation();
+                        const selectedMode = $(this).data('value');
+
+                        if (selectedMode) {
+                            const urlParams = new URLSearchParams(window.location.search);
+                            urlParams.delete('_');
+                            urlParams.delete('isAjax');
+                            urlParams.delete(self.options.page);
+                            urlParams.delete(self.options.limit);
+
+                            urlParams.set(self.options.mode, selectedMode);
+
+                            const baseUrl = window.location.protocol + '//' + window.location.host + window.location.pathname;
+                            const queryString = urlParams.toString();
+                            const targetUrl = baseUrl + (queryString.length ? '?' + queryString : '');
+
+                            self.options.url = targetUrl;
+                            navigationPool.navigate(targetUrl, false);
+                        }
+                    });
+
+                // 3. Sort By dropdown selection handling
+                this.element.find(this.options.orderControl).off('change').on('change', function (e) {
+                    e.preventDefault();
+                    e.stopImmediatePropagation();
+                    self.processParamUpdate(self.options.order, $(this).val());
+                });
+
+                // 4. Limiter dropdown selection handling (Show per page)
+                this.element.find(this.options.limitControl).off('change').on('change', function (e) {
+                    e.preventDefault();
+                    e.stopImmediatePropagation();
+                    self.processParamUpdate(self.options.limit, $(this).val());
+                });
+
+                // 5. Sort Direction switcher (Ascending/Descending)
+                this.element.find(this.options.directionControl).off('click').on('click', function (e) {
+                    e.preventDefault();
+                    e.stopImmediatePropagation();
+                    const selectedDirection = $(this).data('value');
+                    if (selectedDirection) {
+                        self.processParamUpdate(self.options.direction, selectedDirection);
+                    }
+                });
+            },
+
+            /**
+             * @param {String} paramName
+             * @param {*} paramValue
+             * @param {*} defaultValue
+             */
             changeUrl: function (paramName, paramValue, defaultValue) {
                 if (!this.options.ajaxNavigation) {
                     return this._super(paramName, paramValue, defaultValue);
                 }
+                this.processParamUpdate(paramName, paramValue);
+            },
 
-                const urlPaths = this.options.url.split('?');
-                const baseUrl = urlPaths[0];
-                const paramData = this.getUrlParams();
+            /**
+             * @param {String} paramName
+             * @param {*} paramValue
+             */
+            processParamUpdate: function (paramName, paramValue) {
+                const urlParams = new URLSearchParams(window.location.search);
 
-                paramData[paramName] = paramValue;
-                if (paramValue === defaultValue) {
-                    delete paramData[paramName];
-                }
+                urlParams.delete('_');
+                urlParams.delete('isAjax');
 
-                delete paramData[this.options.page];
+                urlParams.set(paramName, paramValue);
+                urlParams.delete(this.options.page);
 
-                const paramString = $.param(paramData);
-                const targetUrl = baseUrl + (paramString.length ? '?' + paramString : '');
+                const baseUrl = window.location.protocol + '//' + window.location.host + window.location.pathname;
+                const queryString = urlParams.toString();
+                const targetUrl = baseUrl + (queryString.length ? '?' + queryString : '');
+
+                this.options.url = targetUrl;
 
                 this._ajaxSubmit(targetUrl);
             },
 
+            /**
+             * @param {String} url
+             */
             _ajaxSubmit: function (url) {
-                const self = this;
-
-                $(this.options.productWrapperSelector).addClass('loading');
-
-                $.ajax({
-                    url: url,
-                    type: 'GET',
-                    success: function (res) {
-                        const html = $(res);
-                        const newMainContent = html.find(self.options.mainContentSelector);
-
-                        if (newMainContent.length) {
-                            $(self.options.mainContentSelector).replaceWith(newMainContent);
-                        } else {
-                            const newProducts = html.find(self.options.productWrapperSelector);
-                            if (newProducts.length) {
-                                $(self.options.productWrapperSelector).replaceWith(newProducts);
-                            }
-
-                            html.find(self.options.toolbarSelector).each(function (index) {
-                                $(self.options.toolbarSelector).eq(index).replaceWith($(this));
-                            });
-
-                            const newPagination = html.find(self.options.paginationSelector);
-                            if (newPagination.length) {
-                                $(self.options.paginationSelector).replaceWith(newPagination).show();
-                            } else {
-                                $(self.options.paginationSelector).hide();
-                            }
-                        }
-
-                        window.history.pushState({}, '', url);
-
-                        $('body').trigger('contentUpdated');
-                    },
-                    error: function () {
-                        window.location.href = url;
-                    }
-                });
+                if (!this.options.ajaxNavigation) {
+                    return this._super(url);
+                }
+                navigationPool.navigate(url, false);
             }
         });
 
