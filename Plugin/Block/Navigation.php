4da8e9ffe7622156c4af81a49ac68bc48ce57d87
buhmann <buhmann81@gmail.com>
2026-07-01 15:05:39 +0200

    cange main filters view template if Ajax Navigation is enabled

--- Plugin/Block/Navigation.php
+++ Plugin/Block/Navigation.php
@@ -1,8 +1,12 @@
 <?php
+/**
+ * Copyright © Buhmann. All rights reserved.
+ */
+declare(strict_types=1);
 
 namespace Buhmann\Catalog\Plugin\Block;
 
-use Smile\ElasticsuiteCatalog\Block\Navigation as SubjectBlock;
+use Magento\LayeredNavigation\Block\Navigation as SubjectBlock;
 use Buhmann\Catalog\ViewModel\LayeredNavigation as CatalogViewModel;
 
 class Navigation
@@ -21,18 +25,60 @@ class Navigation
     }
 
     /**
-     * Dynamically override layout template based on custom AJAX configuration state
+     * Switch template to custom AJAX component if AJAX navigation is enabled
      *
      * @param SubjectBlock $subject
-     * @param callable $proceed
+     * @param string $result
      * @return string
      */
-    public function aroundGetTemplate(SubjectBlock $subject, callable $proceed)
+    public function afterGetTemplate(SubjectBlock $subject, string $result): string
     {
         if ($this->viewModel->isAjaxNavEnabled()) {
+            $subject->setData('viewModel', $this->viewModel);
+            $subject->setData('active', $this->getActiveFilters($subject));
             return 'Buhmann_Catalog::layer/view.phtml';
         }
 
-        return $proceed();
+        return $result;
+    }
+
+    /**
+     * Return index of the facets that are expanded for the current page
+     *
+     * @param SubjectBlock $subject
+     * @return string
+     */
+    private function getActiveFilters(SubjectBlock $subject): string
+    {
+        $activeFilters = [];
+
+        $requestParams = array_keys($subject->getRequest()->getParams());
+        $displayedFilters = $this->getDisplayedFilters($subject->getFilters());
+
+        foreach ($displayedFilters as $index => $filter) {
+            if (in_array($filter->getRequestVar(), $requestParams)) {
+                $activeFilters[] = $index;
+            }
+        }
+
+        return json_encode($activeFilters);
+    }
+
+    /**
+     * Returns facet that are displayed.
+     *
+     * @param array $filters
+     * @return array
+     */
+    private function getDisplayedFilters(array $filters): array
+    {
+        $displayedFilters = array_filter(
+            $filters,
+            function ($filter) {
+                return $filter->getItemsCount() > 0;
+            }
+        );
+
+        return array_values($displayedFilters);
     }
 }
